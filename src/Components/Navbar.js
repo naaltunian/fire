24a2e92419a6b5f51fb4769f6c1842665988ebf7
naaltunian <naaltunian@icloud.com>
2018-10-20 10:00:24 -0400

    add logo placeholder

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = (props) => {
     return(
         <nav className="navbar navbar-dark bg-primary">
-            
+            <img className="navbar-brand" alt="img"></img>
         </nav>
     )
 }
